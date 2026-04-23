256825210212be0705521763adc4dd0acacf0d5c
CSM-800 <33550372+CSM-800@users.noreply.github.com>
2026-04-23 14:56:51 +0200

    Create MESO Reisen user script
    Initial creation of the MESO Reisen user script to open all entries in the travel history.

---
+++ MESO Reisen/meso-reisen.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name				MESO Reisen
+// @namespace		https://camera-obscura.agency
+// @version			1.01
+// @description	Öffnet alle Einträge im Reiseverlauf
+// @author			camera obscura
+// @match				https://www.meso-berlin.de/*
+// @icon				https://www.meso-berlin.de/favicons/apple-touch-icon.png
+// @grant				GM_addStyle
+// @updateURL		https://camera-obscura.agency/userscripts/meso-reisen.user.js
+// @downloadURL	https://camera-obscura.agency/userscripts/meso-reisen.user.js
+// ==/UserScript==
+
+// Changelog
+// https://camera-obscura.agency/userscripts/meso-reisen.txt
+
+GM_addStyle(".trs_txt_bx { display: block; }");
