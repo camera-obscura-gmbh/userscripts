6cf79c216a5820ede97b1aa3966de8e0b8a1d416
CSM-800 <33550372+CSM-800@users.noreply.github.com>
2026-04-23 15:07:40 +0200

    Format user script metadata for consistency

--- meso-reisen/meso-reisen.user.js
+++ meso-reisen/meso-reisen.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
-// @name				MESO Reisen
-// @namespace		https://camera-obscura.agency
-// @version			1.01
-// @description	Öffnet alle Einträge im Reiseverlauf
-// @author			camera obscura
-// @match				https://www.meso-berlin.de/*
-// @icon				https://www.meso-berlin.de/favicons/apple-touch-icon.png
-// @grant				GM_addStyle
-// @updateURL		https://camera-obscura.agency/userscripts/meso-reisen.user.js
-// @downloadURL	https://camera-obscura.agency/userscripts/meso-reisen.user.js
+// @name         MESO Reisen
+// @namespace    https://camera-obscura.agency
+// @version      1.01
+// @description  Öffnet alle Einträge im Reiseverlauf
+// @author       camera obscura
+// @match        https://www.meso-berlin.de/*
+// @icon         https://www.meso-berlin.de/favicons/apple-touch-icon.png
+// @grant        GM_addStyle
+// @updateURL    https://camera-obscura.agency/userscripts/meso-reisen.user.js
+// @downloadURL  https://camera-obscura.agency/userscripts/meso-reisen.user.js
 // ==/UserScript==
 
 // Changelog
