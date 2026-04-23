391a26ea9c2939080309eab148e955f3eda5eecf
CSM-800 <33550372+CSM-800@users.noreply.github.com>
2026-04-23 15:42:28 +0200

    Update URLs to use lowercase in user script

--- meso-reisen/meso-reisen.user.js
+++ meso-reisen/meso-reisen.user.js
@@ -7,9 +7,9 @@
 // @match        https://www.meso-berlin.de/*
 // @icon         https://www.meso-berlin.de/favicons/apple-touch-icon.png
 // @grant        GM_addStyle
-// @updateURL    https://github.com/camera-obscura-gmbh/Userscripts/raw/refs/heads/main/meso-reisen/meso-reisen.user.js
-// @downloadURL  https://github.com/camera-obscura-gmbh/Userscripts/raw/refs/heads/main/meso-reisen/meso-reisen.user.js
-// @supportURL   https://github.com/camera-obscura-gmbh/Userscripts/blob/main/meso-reisen/CHANGELOG.md
+// @updateURL    https://github.com/camera-obscura-gmbh/userscripts/raw/refs/heads/main/meso-reisen/meso-reisen.user.js
+// @downloadURL  https://github.com/camera-obscura-gmbh/userscripts/raw/refs/heads/main/meso-reisen/meso-reisen.user.js
+// @supportURL   https://github.com/camera-obscura-gmbh/userscripts/blob/main/meso-reisen/CHANGELOG.md
 // ==/UserScript==
 
 GM_addStyle(".trs_txt_bx { display: block; }");
